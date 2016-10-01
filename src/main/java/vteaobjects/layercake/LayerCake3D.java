fc0b0a9761cd81f69d14ea3aef68829e18c60756
Seth Winfree <winfrees@iu.edu>
2016-09-30 20:03:18 -0400

    remove old minimal ij1 framework

--- src/main/java/vteaobjects/layercake/LayerCake3D.java
+++ src/main/java/vteaobjects/layercake/LayerCake3D.java
@@ -667,7 +667,6 @@ public class LayerCake3D extends Object implements Cloneable, java.io.Serializab
             //limited to four threads, odd unreproducible behaviour above 4 threads
             int processors = 4;
             int length = alRegions.size() / processors;
-            //int remainder = alRegions.size()%processors; 
 
             if (alRegions.size() < processors) {
                 length = alRegions.size();
