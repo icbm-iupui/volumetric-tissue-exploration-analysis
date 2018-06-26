af1ee4bd4ea9c9fde775da4435e968f6dde78e31
drewnutt <35881828+drewnutt@users.noreply.github.com>
2018-06-26 17:16:20 -0400

    added k-means clustering

---
+++ src/main/java/vtea/clustering/KMeans.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2018 SciJava
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package vtea.clustering;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import org.scijava.plugin.Plugin;
+import vtea.featureprocessing.AbstractFeatureProcessing;
+import vtea.featureprocessing.FeatureProcessing;
+
+/**
+ *
+ * @author drewmcnutt
+ */
+@Plugin (type = FeatureProcessing.class)
+public class KMeans extends AbstractFeatureProcessing{
+    public KMeans(){
+        VERSION = "0.1";
+        AUTHOR = "Andrew McNutt";
+        COMMENT = "Implementation of K-means";
+        NAME = "K-means Clustering";
+        KEY = "Kmeans";
+        TYPE = "Cluster";
+    }
+    
+    public KMeans(int max){
+        VERSION = "0.1";
+        AUTHOR = "Andrew McNutt";
+        COMMENT = "Implementation of K-means";
+        NAME = "K-means Clustering";
+        KEY = "Kmeans";
+        TYPE = "Cluster";
+        
+        protocol = new ArrayList();
+        
+        protocol.add(new JLabel("Number of clusters"));
+       
+        protocol.add(new JSpinner(new SpinnerNumberModel(5,2,max,1)));
+    }
+    
+    @Override
+    public boolean process(ArrayList al, double[][] feature){
+        int n_clust;
+        int n; int dim;
+        ArrayList clusters;
+        
+        ArrayList selectData = (ArrayList)al.get(0);
+        feature = selectColumns(feature, selectData);
+        dataResult.ensureCapacity(feature.length);
+        
+        JSpinner clust = (JSpinner)al.get(4);
+        n_clust = ((Integer)clust.getValue());
+        
+        n = feature.length;
+        dim = feature[0].length;
+        clusters = new ArrayList(n_clust);
+        long randomSeed = System.nanoTime();
+        Random randomGen = new Random(randomSeed);
+        
+        for(int i = 0; i < n_clust; i++)
+            clusters.add(new Centroid(feature[randomGen.nextInt(n)]));
+        int[] oldmembership = new int[n];  
+        Arrays.fill(oldmembership, Integer.MIN_VALUE);
+        int[] membership = new int[n];
+        while(oldmembership != membership){
+            oldmembership = membership;
+            for(int i = 0; i < n; i++){
+                double dist = Double.MAX_VALUE;
+                for(int j = 0; j < n_clust; j++){
+                    double d = calculateDistance(feature[i],((Centroid)clusters.get(j)).getLocation());
+                    if(d < dist){
+                        membership[i] = j;
+                        dist = d;
+                    }
+
+                }
+            }
+
+            for(int i = 0; i < n_clust; i++){
+                int count = 0;
+                ArrayList center = new ArrayList(dim);
+                for(int cent = 0; cent < dim; cent++)
+                    center.add((double)0.0);
+                for(int j = 0; j < n; j++){
+                    if(membership[j] == i){
+                        count++;
+                        for(int k = 0; k < dim; k++){
+                            center.set(k, (double)center.get(k) + feature[j][k]);
+                        }
+                    }
+                }
+                double[] newCentroid = new double[dim];
+                for(int k = 0; k < dim; k++)
+                    newCentroid[k] = (double)center.get(k)/count;
+
+                clusters.set(i, new Centroid(newCentroid));
+
+            }
+        }
+        for(int memb: membership)
+            dataResult.add(memb);
+        return true;
+    }
+    
+    @Override
+    public ArrayList getBlockCommentLocation(){
+        ArrayList comment = new ArrayList();
+        comment.add(0);
+        comment.add(0);
+        comment.add(1);
+        comment.add(1);
+        
+        return comment;
+    }
+    
+    class Centroid{
+        double[] location;
+        
+        Centroid(double[] coord){
+            location = coord;
+        }
+        
+        public double[] getLocation(){
+            return location;
+        }
+    }
+    
+    double calculateDistance(double[] n1, double[] n2){
+        double dist = 0;
+        if(n1.length != n2.length)
+            throw new RuntimeException("The dimensions of the two vectors are not the same");
+        
+        int dim = n1.length;
+        
+        for(int i = 0; i < dim; i++){
+            dist += (n1[i] - n2[i]) * (n1[i] - n2[i]);
+        }
+        
+        return dist;
+    }
+}
