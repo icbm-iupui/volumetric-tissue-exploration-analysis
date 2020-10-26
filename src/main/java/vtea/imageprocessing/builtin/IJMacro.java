40836aaf240fd4b748b8a05d64920b57616dc7c2
Seth Winfree <winfrees@iu.edu>
2020-10-26 10:49:34 -0400

    Merge origin/ReEngineerLUTImplementation into ReEngineerLUTImplementation

--- src/main/java/vtea/imageprocessing/builtin/IJMacro.java
+++ src/main/java/vtea/imageprocessing/builtin/IJMacro.java
@@ -17,17 +17,26 @@
  */
 package vtea.imageprocessing.builtin;
 
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.process.ImageProcessor;
+import ij.process.StackProcessor;
 import java.awt.Dimension;
 import java.util.ArrayList;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import org.scijava.plugin.Plugin;
 import vtea.imageprocessing.AbstractImageProcessing;
+import vtea.imageprocessing.ImageProcessing;
 
 /**
  *
  * @author sethwinfree
  */
-//@Plugin(type = ImageProcessing.class)
+//@Plugin(type = ImageProcessing.class)  still needs some work, fix load .ijm
 public class IJMacro extends AbstractImageProcessing {
 
     public IJMacro() {
@@ -48,10 +57,79 @@ public class IJMacro extends AbstractImageProcessing {
         paneScrollPane.setMinimumSize(new Dimension(250, 155));
         paneScrollPane.setToolTipText("Add IJ macro text here");
 
-        //protocol.add(new JLabel("Paste macro here:"));
+        protocol.add(new JLabel("Write macro\nhere:"));
         protocol.add(paneScrollPane);
+        protocol.add(scriptText);
+        
 
         //final ImageJ ij = new ImageJ();
 ////        ij.op().labeling().cca(thresholded, StructuringElement.FOUR_CONNECTED);
+
+
+    }
+    
+    @Override
+    public boolean process(ArrayList al, ImagePlus imp) {
+        JScrollPane pane = (JScrollPane) al.get(3);
+        JTextPane scriptText = (JTextPane)pane.getComponent(0);
+
+        String macro = scriptText.getText();
+        imp.show();
+        IJ.runMacro(macro);
+        imp.hide();
+
+        return true;
+    }
+    
+     @Override
+    public boolean copyComponentParameter(String version, ArrayList dComponents, ArrayList sComponents) {
+
+        try {
+            dComponents.clear();
+
+            JTextPane script = (JTextPane) sComponents.get(1);
+            dComponents.add(new JLabel("Write Macro here:"));
+            dComponents.add(new JScrollPane(script));
+
+            return true;
+        } catch (Exception e) {
+            System.out.println("ERROR: Could not copy parameter(s) for " + NAME);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean loadComponentParameter(String version, ArrayList dComponents, ArrayList fields) {
+        try {
+            dComponents.clear();
+
+            String script = (String) fields.get(0);
+
+            dComponents.add(new JLabel("Write Macro here:"));
+            //dComponents.add((new JScrollPane(new JTextPane(script))));
+
+            return true;
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Could not copy parameter(s) for " + NAME);
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean saveComponentParameter(String version, ArrayList dComponents, ArrayList fields) {
+        try {
+            fields.clear();
+            fields.add(((JTextField) (dComponents.get(1))).getText());
+            return true;
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Could not copy parameter(s) for " + NAME);
+
+            return false;
+        }
     }
 }
